e4ff19609a1375da686ea97660ce7dd6c20db514
Kenjinh <nohara0312@gmail.com>
2024-06-04 21:37:12 -0300

    feat: Add table path
    - Load .env
    - Load dataframe on html

--- app.py
+++ app.py
@@ -1,27 +1,26 @@
 from flask import Flask, render_template
 import pandas as pd
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 
-##########################################################
-########################### 01 ###########################
-##########################################################
+# Carrega o arquivo .env
+load_dotenv()
+
 @app.route('/')
 def index():
     return render_template('index.html')
-
-
-##########################################################
-########################### 02 ###########################
-##########################################################
-# CRIANDO O DATAFRAME
+    
 df = pd.DataFrame({
     'alunos': ['Renato', 'Fernando', 'Rodrigo', 'Ana', 'Joana', 'Silvio', 'Carolina'],
     'notas': [15.00, 39.58, 62.92, 41.46, 48.33, 63.13, 70.00]
 })
 
-# RENDERIZE OS VALORES DO DATAFRAME df EM UMA TABELA HTML DENTRO DA PÁGINA /table.html (CRIE UM HTML PARA ISSO)
 @app.route('/table')
 def table():
+    # Renderiza o html na tela com os dados das notas
+    return render_template('table.html', notas=[df.to_html(classes='data')])
 
-    return render_template('table.html', )
+if __name__ == '__main__':
+    # Aplica o Flask na porta 5000
+    app.run(port=5000)
